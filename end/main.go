6b2297424515fabc2cb5ffe5b00d2402d2362582
1900018250 <1900018250@qq.com>
2020-05-27 23:12:06 +0800

    备份

--- end/main.go
+++ end/main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"time"
 	
-	"./config"
+	"./configs"
 	"./router"
 	
 	"github.com/gin-gonic/gin"
@@ -15,14 +15,14 @@ import (
 )
 
 var (
-	cfg = pflag.StringP("config", "c", "", "apiserver config file path.")
+	cfg = pflag.StringP("configs", "c", "", "apiserver configs file path.")
 )
 
 func main() {
 	pflag.Parse()
 	
-	// init config
-	if err := config.Init(*cfg); err != nil {
+	// init configs
+	if err := configs.Init(*cfg); err != nil {
 		panic(err)
 	}
 	
